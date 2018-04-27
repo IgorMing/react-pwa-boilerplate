4740e5eb4cc64f8f4caa7d5b281777818e0bd11f
Igor Ming de Mesquita <igor.mesquita@s2it.com.br>
2018-04-27 19:16:00 -0300

    Added babel eslint dependency and removed unused import.

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
 
 import { loginAction } from 'components/Auth/duck';
 import LoginForm from 'components/LoginForm';
@@ -9,7 +8,6 @@ import LoginForm from 'components/LoginForm';
 const mapDispatchToProps = { login: loginAction };
 
 @connect(null, mapDispatchToProps)
-@withRouter
 export default class Login extends Component {
   static propTypes = {
     login: PropTypes.func.isRequired
